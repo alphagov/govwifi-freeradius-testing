91776a0e04bf2bd6a78443b9a96db044e692a4e6
Jim Narey <jimnarey@me.com>
2022-08-13 14:37:05 +0100

    Add basic HTTP server to receive REST module output

---
+++ rest_receiver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import simplejson
+
+HOST_NAME = "localhost"
+PORT = 8081
+
+
+def print_request_data(data_string):
+    print('*'*60)
+    print('Data String: {}'.format(data_string.decode('utf-8')))
+    print('Data JSON: {}'.format(simplejson.loads(data_string)))
+
+
+class RestReceiver(BaseHTTPRequestHandler):
+    def do_POST(self):
+        try:
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            print_request_data(data_string)
+            self.send_response(200)
+            self.end_headers()      
+        except Exception as e:
+            print(e)
+            self.send_response(500)
+            self.end_headers()
+
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((HOST_NAME, PORT), RestReceiver)
+    print('Server started http://{0}:{1}'.format(HOST_NAME, PORT))
+
+    try:
+        webServer.serve_forever()
+    except:
+        pass
+
+    webServer.server_close()
+    print('\nServer stopped.')
+
